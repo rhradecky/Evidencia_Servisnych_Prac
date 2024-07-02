c842f9db0ca0c894f3b43583331c5932d7d46ca3
hradecky <rhradecky@gmail.com>
2024-07-02 10:39:58 +0200

    Merge remote-tracking branch 'origin/master'

--- load_files2.py
+++ load_files2.py
@@ -1,9 +1,11 @@
 import os
+import csv
+from datetime import datetime
 
 def spracuj_nazov_suboru(nazov):
     serial_number = nazov[:10]
-    firmware = nazov[11:15]  # Znaky 12 až 15 (indexovanie je 0-based)
-    return serial_number, firmware
+    fw = nazov[11:15]  # Znaky 12 až 15 (indexovanie je 0-based)
+    return serial_number, fw
 
 def vytvor_datovu_strukturu(adresar):
     try:
@@ -17,15 +19,29 @@ def vytvor_datovu_strukturu(adresar):
         directory = []
         spracovane_serial_numbers = set()
         for subor in len_subory:
-            serial_number, firmware = spracuj_nazov_suboru(subor)
+            serial_number, fw = spracuj_nazov_suboru(subor)
             if serial_number == "999ABC9999":
                 continue
             if serial_number not in spracovane_serial_numbers:
-                directory.append({'serial_number': serial_number, 'firmware': firmware})
+                directory.append({
+                    'serial_number': serial_number,
+                    'fw': fw,
+                    'uroven': 'L1',
+                    'faktura': '20240011',
+                    'datum': datetime.now().strftime('%Y-%m-%d')
+                })
                 spracovane_serial_numbers.add(serial_number)
 
+        # Uloženie dátovej štruktúry do CSV súboru
+        csv_soubor = 'vysledky.csv'
+        with open(csv_soubor, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['serial_number', 'fw', 'uroven', 'faktura', 'datum'])
+            writer.writeheader()
+            for item in directory:
+                writer.writerow(item)
+
         # Výpis dátovej štruktúry
-        print("Dátová štruktúra zo súborov v adresári", adresar)
+        print(f"Dátová štruktúra zo súborov v adresári {adresar} bola uložená do súboru {csv_soubor}.")
         for item in directory:
             print(item)
     except FileNotFoundError:
@@ -36,5 +52,5 @@ def vytvor_datovu_strukturu(adresar):
         print("Vyskytla sa chyba:", e)
 
 # Príklad použitia
-adresar = "C:/Users/hrade/PycharmProjects/myproject/Evidencia_Servisnych_Prac/serial_number"
+adresar = "C:\Radiopol_3"
 vytvor_datovu_strukturu(adresar)
